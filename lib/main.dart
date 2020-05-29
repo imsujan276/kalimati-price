1e53337135617c42a1decf0ae15bd82d8aec9016
sujan.qode <sujan.qode@gmail.com>
2020-05-29 19:18:07 +0545

    added webview home page to replace previous one

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:kalimati_price/pages/home_page.dart';
+// import 'package:kalimati_price/pages/home_page.dart';
+import 'package:kalimati_price/pages/home_page_new.dart';
 
 void main(){
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,20 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Kalimati Price',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      home: HomePageNew(),
       debugShowCheckedModeBanner: false,
     );
   }
---
+++ lib/pages/home_page_new.dart
@@ -0,0 +1,85 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:rounded_modal/rounded_modal.dart';
+
+class HomePageNew extends StatefulWidget {
+  @override
+  _HomePageNewState createState() => _HomePageNewState();
+}
+
+class _HomePageNewState extends State<HomePageNew> {
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Kalimati Price Today"),
+        elevation: 0.0,
+        actions: <Widget>[
+          new IconButton(
+              icon: new Icon(
+                Icons.help,
+                color: Colors.white,
+              ),
+              onPressed: () => {showBottomSheetModal()})
+        ],
+      ),
+      body: Builder(builder: (BuildContext context) {
+        return WebView(
+          initialUrl: 'https://nepalicalendar.rat32.com/vegetable/embed.php',
+          javascriptMode: JavascriptMode.unrestricted,
+          onWebViewCreated: (WebViewController webViewController) {
+            _controller.complete(webViewController);
+          },
+          onPageStarted: (String url) {
+            print('Page started loading: $url');
+          },
+          onPageFinished: (String url) {
+            print('Page finished loading: $url');
+          },
+          gestureNavigationEnabled: true,
+        );
+      }),
+    );
+  }
+
+  showBottomSheetModal() {
+    showRoundedModalBottomSheet(
+      context: context,
+      radius: 10.0, // This is the default
+      color: Colors.white, // Also default
+      builder: (context) => Container(
+          child: Container(
+        child: Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Column(
+            children: <Widget>[
+              Text('About App',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w500,
+                  )),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                "Kalimati Price is the app made to know the todays's price of Kalimati.",
+                style: TextStyle(fontSize: 16.0),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                "The prices are updated daily. So, no need to wait for the price update from news.",
+                style: TextStyle(fontSize: 16.0),
+              )
+            ],
+          ),
+        ),
+      )),
+    );
+  }
+}
